373489dc8afad771a763876430409ca28c20319e
zakariahaniya <zakalvha@gmail.com>
2024-09-25 08:13:59 +0700

    ternary pertama

--- PemilihanBilangan16.java
+++ PemilihanBilangan16.java
@@ -8,10 +8,8 @@ public class PemilihanBilangan16 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
-        if (angka % 2 == 0) {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        } else {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String hasil;
+        hasil = (angka % 2 == 0) ? "genap" : "ganjil";
+        System.out.println("Angka " + angka + " termasuk bilangan " + hasil); 
     }
 }
